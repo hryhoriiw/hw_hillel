e6fed705d01815b804d819b5b0d83880d4479f21
hryhorii harkusha <workhryhorii@gmail.com>
2024-01-27 20:06:33 +0200

    Added hw-2(2)

---
+++ hw-2(2)/app.js
@@ -0,0 +1,5 @@
+const firstString = prompt('write the first string');
+const secondString = prompt('write the first string');
+const thirdString = prompt('write the first string');
+
+alert(firstString+ ' ' + secondString + ' ' + thirdString)
