fcc4c5ef194427f57048bae4055e8ad1a5cdf54e
hryhorii harkusha <workhryhorii@gmail.com>
2024-01-27 20:07:39 +0200

    Added hw-2

---
+++ hw-2/app.js
@@ -0,0 +1,11 @@
+const fiveNumber = prompt('enter a five-digit number')
+ 
+const digit1 = parseInt(fiveNumber[0])
+const digit2 = parseInt(fiveNumber[1])
+const digit3 = parseInt(fiveNumber[2])
+const digit4 = parseInt(fiveNumber[3])
+const digit5 = parseInt(fiveNumber[4])
+
+const userResult = digit1 + ' ' + digit2 + ' ' + digit3 + ' ' + digit4 + ' ' + digit5;
+
+alert('your result = ' + userResult);
