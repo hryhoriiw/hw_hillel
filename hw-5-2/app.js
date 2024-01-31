03fd8c7321b610628cafa2c17321831c74dc9b00
hryhorii harkusha <workhryhorii@gmail.com>
2024-01-31 18:53:00 +0200

    Added hw-5-2

---
+++ hw-5-2/app.js
@@ -0,0 +1,90 @@
+const howManyYears = parseInt(prompt('Скільки тобі років?')); //how many
+
+if (howManyYears) {
+    alert('Тобі зараз ' + howManyYears + ' років');
+} else {
+    alert('Шкода, що Ви не захотіли ввести свій вік..')
+}
+
+const whereFrom = prompt('Звідки ти?'); //where
+
+const whereFromCapital = {
+    'Київ': 'Україна',
+    'Вашингтон': 'США',
+    'Лондон': 'Велика Британія'
+};
+
+let isСapital = whereFrom === 'Київ' || whereFrom === 'Вашингтон' || whereFrom === 'Лондон';
+
+let county = isСapital ? whereFromCapital[whereFrom] : '';
+
+if (isСapital) {
+    alert('Ти проживаєш у країні ' + county + ' в місті ' + whereFrom)
+} else if (whereFrom){
+    alert('Ти проживаєш у місті ' + whereFrom)
+} else {
+    alert('Шкода, що Ви не захотіли ввести своє місто..')
+}
+
+const favoriteSport = prompt('Який в тебе найулюбленеший вид спорту?'); //sport
+
+
+const favoriteSprotMatch = {
+    'Футбол': 'Круто! Хочеш стати як Шевченко?',
+    'Баскетбол': 'Круто! Хочеш стати як Нікола Йокіч?',
+    'Теніс': 'Круто! Хочеш стати як Карлос Алькарас?'
+};
+
+let sportMatch = favoriteSport === 'Футбол' || favoriteSport === 'Баскетбол' || favoriteSport === 'Теніс';
+
+let sportMatchСoincided = sportMatch ? favoriteSprotMatch[favoriteSport] : '';
+
+if (sportMatch) {
+    alert(sportMatchСoincided)
+} else if (favoriteSport){
+    alert('Хороший вибір!')
+} else {
+    alert('Шкода, що Ви не захотіли ввести свій найулюбленеший вид спорту..')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const howManyYears = parseInt(prompt('Скільки тобі років?'));
+// const whereFrom = prompt('Звідки ти?');
+// const favoriteSport = prompt('Який в тебе найулюбленіший вид спорту?');
+
+// alert('Тобі зараз ' + howManyYears + ' років');
+
+// const capitalCountryPairs = {
+//     'Київ': 'Україна',
+//     'Вашингтон': 'США',
+//     'Лондон': 'Велика Британія'
+// };
+
+// let isCapital = whereFrom === 'Київ' || whereFrom === 'Вашингтон' || whereFrom === 'Лондон';
+// let country = isCapital ? capitalCountryPairs[whereFrom] : '';
+
+// if (isCapital) {
+//     alert('Ти живеш у столиці ' + whereFrom + ', країна - ' + country);
+// } else {
+//     alert('Ти живеш у місті ' + whereFrom);
+// }
+
+
